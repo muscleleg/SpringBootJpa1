54fcae116c1fb7fde81dbec8107c098bb6f02d72
jaejun <aamoof@gmail.com>
2022-07-21 22:22:22 +0900

    회원 기능 테스트(미완)

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -2,39 +2,39 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
-@Transactional(readOnly = true) //기본값이 false임, 해담 MemberRepository클래스에는 읽는 메소드가 많기때문에 전체에 ture를 먹인후 아닌것들에만 false를 먹이는 느낌으로 적용한것
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class MemberService {
-    private MemberRepository memberRepository;
-
+    private final MemberRepository memberRepository;
     /**
-     * 회원 가입
+     * 회원가입
      */
-    @Transactional
+    @Transactional //변경
     public Long join(Member member) {
-
-        validateDuplicateMember(member);//중복회원검증
+        validateDuplicateMember(member); //중복 회원 검증
         memberRepository.save(member);
         return member.getId();
     }
-
     private void validateDuplicateMember(Member member) {
-        List<Member> findMembers = memberRepository.findByName(member.getName());
+        List<Member> findMembers =
+                memberRepository.findByName(member.getName());
         if (!findMembers.isEmpty()) {
-            throw new IllegalStateException("이미 존재하는 회원입니다.")
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
         }
-        //Exception
     }
-    //회원 전체 조회
+    /**
+     * 전체 회원 조회
+     */
     public List<Member> findMembers() {
         return memberRepository.findAll();
     }
-
     public Member findOne(Long memberId) {
         return memberRepository.findOne(memberId);
     }
